219a8d6a94ecd1753dd5e0efebe5ebc89a353d20
= <chris.d.nixon@gmail.com>
2024-10-20 11:45:44 -0700

    Remove spacing

--- cs2-setup.sh
+++ cs2-setup.sh
@@ -82,91 +82,91 @@ sudo -i -u $STEAM_USER env AWS_REGION="$AWS_REGION" \
   RCON_PASSWORD="$RCON_PASSWORD" \
   DISCORD_WEBHOOK="$DISCORD_WEBHOOK" bash <<'EOF'
 
-  cd $CS2_DIR
+cd $CS2_DIR
 
-  echo "I am acting as $(whoami)"
+echo "I am acting as $(whoami)"
 
-  echo "Starting install of linuxgsm."
-  curl -Lo linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh cs2server
-  ./cs2server auto-install
-  echo "Completed install of linuxgsm."
+echo "Starting install of linuxgsm."
+curl -Lo linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh cs2server
+./cs2server auto-install
+echo "Completed install of linuxgsm."
 
-  # Install Metamod
-  echo "Installing metamod."
-  wget -q -O /tmp/metamod.tar.gz "$METAMOD_URL"
-  tar -xzf /tmp/metamod.tar.gz -C "$CSGO_GAME_DIR"
-  rm -f /tmp/metamod.tar.gz
-  if grep -Fxq "$METAMOD_GAMEINFO_ENTRY" "$GAMEINFO_FILE_PATH"; then
-    echo "The entry '$METAMOD_GAMEINFO_ENTRY' already exists in ${GAMEINFO_FILE_PATH}. No changes were made."
-  else
-    awk -v new_entry="$METAMOD_GAMEINFO_ENTRY" '
-        BEGIN { found=0; }
-        // {
-            if (found) {
-                print new_entry;
-                found=0;
-            }
-            print;
-        }
-        /Game_LowViolence/ { found=1; }
-    ' "$GAMEINFO_FILE_PATH" >"$GAMEINFO_FILE_PATH.tmp" && mv "$GAMEINFO_FILE_PATH.tmp" "$GAMEINFO_FILE_PATH"
-    echo "The file ${GAMEINFO_FILE_PATH} has been modified successfully. '$METAMOD_GAMEINFO_ENTRY' has been added."
-  fi
-  echo "Completed installing metamod."
+# Install Metamod
+echo "Installing metamod."
+wget -q -O /tmp/metamod.tar.gz "$METAMOD_URL"
+tar -xzf /tmp/metamod.tar.gz -C "$CSGO_GAME_DIR"
+rm -f /tmp/metamod.tar.gz
+if grep -Fxq "$METAMOD_GAMEINFO_ENTRY" "$GAMEINFO_FILE_PATH"; then
+  echo "The entry '$METAMOD_GAMEINFO_ENTRY' already exists in ${GAMEINFO_FILE_PATH}. No changes were made."
+else
+  awk -v new_entry="$METAMOD_GAMEINFO_ENTRY" '
+      BEGIN { found=0; }
+      // {
+          if (found) {
+              print new_entry;
+              found=0;
+          }
+          print;
+      }
+      /Game_LowViolence/ { found=1; }
+  ' "$GAMEINFO_FILE_PATH" >"$GAMEINFO_FILE_PATH.tmp" && mv "$GAMEINFO_FILE_PATH.tmp" "$GAMEINFO_FILE_PATH"
+  echo "The file ${GAMEINFO_FILE_PATH} has been modified successfully. '$METAMOD_GAMEINFO_ENTRY' has been added."
+fi
+echo "Completed installing metamod."
 
-  # Install CounterStrikeSharp
-  echo "Installing CounterStrikeSharp."
-  wget -q -O /tmp/cssharp.zip "$COUNTER_STRIKE_SHARP_URL"
-  unzip -qo /tmp/cssharp.zip -d "$CSGO_GAME_DIR"
-  rm -f /tmp/cssharp.zip
-  echo "Completed installing CounterStrikeSharp."
+# Install CounterStrikeSharp
+echo "Installing CounterStrikeSharp."
+wget -q -O /tmp/cssharp.zip "$COUNTER_STRIKE_SHARP_URL"
+unzip -qo /tmp/cssharp.zip -d "$CSGO_GAME_DIR"
+rm -f /tmp/cssharp.zip
+echo "Completed installing CounterStrikeSharp."
 
-  # Install MatchZy
-  echo "Installing MatchZy."
-  wget -q -O /tmp/matchzy.zip "$MATCHZY_URL"
-  unzip -qo /tmp/matchzy.zip -d "$CSGO_GAME_DIR"
-  rm -f /tmp/matchzy.zip
-  echo "Completed installing MatchZy."
+# Install MatchZy
+echo "Installing MatchZy."
+wget -q -O /tmp/matchzy.zip "$MATCHZY_URL"
+unzip -qo /tmp/matchzy.zip -d "$CSGO_GAME_DIR"
+rm -f /tmp/matchzy.zip
+echo "Completed installing MatchZy."
 
-  # Update MatchZy admins
-  sed -i "s/\"76561198154367261\": \".*\"/\"$EAGLE_STEAM_ID\": \"\"/" "$MATCHZY_ADMINS_FILE_PATH"
+# Update MatchZy admins
+sed -i "s/\"76561198154367261\": \".*\"/\"$EAGLE_STEAM_ID\": \"\"/" "$MATCHZY_ADMINS_FILE_PATH"
 
-  # Set knife round time to 69 seconds (1.15 minutes)
-  sed -i "s/^mp_roundtime .*/mp_roundtime 1.15/" "$MATCHZY_KNIFE_CONFIG_FILE_PATH"
-  sed -i "s/^mp_roundtime_defuse .*/mp_roundtime_defuse 1.15/" "$MATCHZY_KNIFE_CONFIG_FILE_PATH"
-  sed -i "s/^mp_roundtime_hostage .*/mp_roundtime_hostage 1.15/" "$MATCHZY_KNIFE_CONFIG_FILE_PATH"
+# Set knife round time to 69 seconds (1.15 minutes)
+sed -i "s/^mp_roundtime .*/mp_roundtime 1.15/" "$MATCHZY_KNIFE_CONFIG_FILE_PATH"
+sed -i "s/^mp_roundtime_defuse .*/mp_roundtime_defuse 1.15/" "$MATCHZY_KNIFE_CONFIG_FILE_PATH"
+sed -i "s/^mp_roundtime_hostage .*/mp_roundtime_hostage 1.15/" "$MATCHZY_KNIFE_CONFIG_FILE_PATH"
 
-  # Replace MatchZy server config with custom GamingHerd config
-  wget -q -O "$MATCHZY_TEMP_SERVER_FILE_PATH" "$GITHUB_MATCHZY_SERVER_CONFIG_URL"
-  mv "$MATCHZY_TEMP_SERVER_FILE_PATH" "$MATCHZY_CONFIG_FILE_PATH"
+# Replace MatchZy server config with custom GamingHerd config
+wget -q -O "$MATCHZY_TEMP_SERVER_FILE_PATH" "$GITHUB_MATCHZY_SERVER_CONFIG_URL"
+mv "$MATCHZY_TEMP_SERVER_FILE_PATH" "$MATCHZY_CONFIG_FILE_PATH"
 
-  # Add MatchZy live_override.cfg
-  wget -q -O "$MATCHZY_TEMP_LIVE_OVERRIDE_FILE_PATH" "$GITHUB_MATCHZY_LIVE_OVERRIDE_CONFIG_URL"
-  mv "$MATCHZY_TEMP_LIVE_OVERRIDE_FILE_PATH" "$MATCHZY_DIR"
+# Add MatchZy live_override.cfg
+wget -q -O "$MATCHZY_TEMP_LIVE_OVERRIDE_FILE_PATH" "$GITHUB_MATCHZY_LIVE_OVERRIDE_CONFIG_URL"
+mv "$MATCHZY_TEMP_LIVE_OVERRIDE_FILE_PATH" "$MATCHZY_DIR"
 
-  # Replace cs2server.cfg with GamingHerd override
-  wget -q -O "$GSM_CS2_SERVER_OVERRIDE_FILE_PATH" "$GSM_CS2_SERVER_OVERRIDE_CONFIG_URL"
-  mv "$GSM_CS2_SERVER_OVERRIDE_FILE_PATH" "$CS2_SERVER_CFG"
+# Replace cs2server.cfg with GamingHerd override
+wget -q -O "$GSM_CS2_SERVER_OVERRIDE_FILE_PATH" "$GSM_CS2_SERVER_OVERRIDE_CONFIG_URL"
+mv "$GSM_CS2_SERVER_OVERRIDE_FILE_PATH" "$CS2_SERVER_CFG"
 
-  sed -i "s/^rcon_password\b.*/rcon_password \"$RCON_PASSWORD\"/" "$CS2_SERVER_CFG"
+sed -i "s/^rcon_password\b.*/rcon_password \"$RCON_PASSWORD\"/" "$CS2_SERVER_CFG"
 
-  # Set up cron jobs
-  CRON_JOBS="*/5 * * * * /home/cs2server/cs2server monitor > /dev/null 2>&1
-  */30 * * * * /home/cs2server/cs2server update > /dev/null 2>&1
-  0 0 * * 0 /home/cs2server/cs2server update-lgsm > /dev/null 2>&1"
+# Set up cron jobs
+CRON_JOBS="*/5 * * * * /home/cs2server/cs2server monitor > /dev/null 2>&1
+*/30 * * * * /home/cs2server/cs2server update > /dev/null 2>&1
+0 0 * * 0 /home/cs2server/cs2server update-lgsm > /dev/null 2>&1"
 
-  crontab -l 2>/dev/null > /tmp/current_cronjobs
-  echo "$CRON_JOBS" >> /tmp/current_cronjobs
-  crontab /tmp/current_cronjobs
-  rm /tmp/current_cronjobs
-  echo "Cron jobs installed."
+crontab -l 2>/dev/null > /tmp/current_cronjobs
+echo "$CRON_JOBS" >> /tmp/current_cronjobs
+crontab /tmp/current_cronjobs
+rm /tmp/current_cronjobs
+echo "Cron jobs installed."
 
-  # Add Discord alerts to LinuxGSM
-  {
-    echo ""
-    echo "discordalert=\"on\""
-    echo "discordwebhook=\"$DISCORD_WEBHOOK\""
-    echo 'startparameters="-dedicated -usercon -ip 0.0.0.0 -port 27015 -maxplayers 12 +game_type 0 +game_mode 1 +exec cs2server.cfg"'
-  } | sudo tee -a "$LINUXGSM_COMMON_CFG"
+# Add Discord alerts to LinuxGSM
+{
+  echo ""
+  echo "discordalert=\"on\""
+  echo "discordwebhook=\"$DISCORD_WEBHOOK\""
+  echo 'startparameters="-dedicated -usercon -ip 0.0.0.0 -port 27015 -maxplayers 12 +game_type 0 +game_mode 1 +exec cs2server.cfg"'
+} | sudo tee -a "$LINUXGSM_COMMON_CFG"
 
 EOF
